061e6eecd92115037249bb795826e0f81564190b
jadens3 <49039591+jadens3@users.noreply.github.com>
2020-03-13 09:53:31 -0700

    Debugging

--- index.md
+++ index.md
@@ -1,11 +1,5 @@
-<head>
-  <title>Test DOM</title>
-  <meta charset="UTF-8">
-</head>
 <body>
-  <div id='data'>Data displayed here</div>
+  <div id='data'></div>
 </body>
-<!-- polyfill -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js"></script>
 <!-- Main Script -->
 <script src="test_dom.js"></script>
--- test_dom.js
+++ test_dom.js
@@ -1,7 +1,4 @@
-'use strict';
-let url = 'https://jsonplaceholder.typicode.com/posts';
-
-fetch(url)
+fetch('https://jsonplaceholder.typicode.com/posts')
   .then(function (response) {
     return response.json();
   })
@@ -14,17 +11,21 @@ fetch(url)
 
   function display(data) {
     var container = document.getElementById('data');
+    var user_container;
+    var data_container;
+    var title_div;
+    var body_div;
     let prev_user = '';
     for(let i = 0; i < data.length; i++) {
       if(data[i].userId != prev_user) {
-        let user_container = document.createElement('div');
+        user_container = document.createElement('div');
         div.innerHTML = 'User ID: ' + data[i].userId;
         container.appendChild(inner_container);
       }
 
-      let data_container = document.createElement('div');
-      let title_div = document.createElement('div');
-      let body_div = document.createElement('div');
+      data_container = document.createElement('div');
+      title_div = document.createElement('div');
+      body_div = document.createElement('div');
 
       title_div.innerHTML = data[i].id + '. ' + data[i].title;
       body_div.innerHTML = data[i].body;
